7f47622ea856d791d8ac44ce30a33ed6ffcefefe
LouiseChili <chililove@users.noreply.github.com>
2025-05-10 16:27:46 +0200

    setting up cryptolist component

--- src/app/components/crypto-list/crypto-list.component.ts
+++ src/app/components/crypto-list/crypto-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CryptoService, Coin } from 'src/app/services/crypto.service';
 
 @Component({
   selector: 'app-crypto-list',
   templateUrl: './crypto-list.component.html',
   styleUrls: ['./crypto-list.component.css']
 })
-export class CryptoListComponent {
+
+export class CryptoListComponent implements OnInit {
+  coins: Coin[] = [];
+  isLoading = true;
+  errorMessage = '';
+
+  constructor(private cryptoService: CryptoService) {}
+
+  ngOnInit(): void {
+    this.cryptoService.getCoins().subscribe({
+      next: (data) => {
+        this.coins = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Noget gik galt, smt went wrong with api call';
+        console.error(error);
+        this.isLoading = false;
+      }
+    });
+  }
 
 }
