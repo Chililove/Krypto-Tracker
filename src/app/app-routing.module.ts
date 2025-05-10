e60416c534e0292118b1cc3bc12bc62226caa124
LouiseChili <chililove@users.noreply.github.com>
2025-05-10 16:24:45 +0200

    setting up router w cryptolist path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { CryptoDetailComponent } from './components/crypto-detail/crypto-detail.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: CryptoListComponent},
+  { path: 'crypto-list', component: CryptoListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
