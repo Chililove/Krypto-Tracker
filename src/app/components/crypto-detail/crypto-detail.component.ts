8cf64ef22ba3804acb809ccca0cc8715ed6ca597
LouiseChili <chililove@users.noreply.github.com>
2025-05-14 11:54:34 +0200

    Creating Coin-Detail component to show all info on coin

--- src/app/components/crypto-detail/crypto-detail.component.ts
+++ src/app/components/crypto-detail/crypto-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CryptoService, CoinDetail } from '../../services/crypto.service';
 
 @Component({
   selector: 'app-crypto-detail',
   templateUrl: './crypto-detail.component.html',
   styleUrls: ['./crypto-detail.component.css']
 })
-export class CryptoDetailComponent {
+export class CryptoDetailComponent implements OnInit {
+  coinId: string = '';
+  coin: CoinDetail | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private cryptoService: CryptoService
+  ) {}
+
+  ngOnInit(): void {
+    this.coinId = this.route.snapshot.paramMap.get('id') || '';
+    this.cryptoService.getCoin(this.coinId).subscribe({
+      next: (data) => {
+        this.coin = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
 }
